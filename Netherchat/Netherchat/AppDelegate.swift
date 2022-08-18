e2f8e281a3fa126a5f7a549da6b477d28f577e85
salehkreiner <salehkreiner@gmail.com>
2022-08-18 16:00:37 -0700

    Add login screen

---
+++ Netherchat/Netherchat/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Netherchat
+//
+//  Created by Salty Dog on 8/9/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        //window?.backgroundColor = UIColor(red:22.0, green:3.0, blue:87.0, alpha: 1.00)
+        window?.backgroundColor = .systemIndigo
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+
+}
+
